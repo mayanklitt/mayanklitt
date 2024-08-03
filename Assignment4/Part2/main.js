4f6a1320e2006569a1ce31b6c79963c1d10e8073
mayanklitt <mayankkumar1@dcmail.ca>
2024-08-02 20:24:53 -0400

    Create loop through image
    - Loop through image

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -22,7 +22,16 @@ const alts = {
   }
 
 /* Looping through images */
-
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 
 /* Wiring up the Darken/Lighten button */
 
