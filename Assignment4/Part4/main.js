cc078701033e7a395c757b077745e2c1984cc6a5
mayanklitt <mayankkumar1@dcmail.ca>
2024-08-03 12:38:19 -0400

    Ball classs updated
    - Update the ball class

--- Assignment4/Part4/main.js
+++ Assignment4/Part4/main.js
@@ -33,14 +33,14 @@ class Shape {
 
 }
 
-class Ball {
+class Ball extends Shape {
+
   constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
+    super(x, y, velX, velY);
+
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -51,39 +51,41 @@ class Ball {
   }
 
   update() {
-    if (this.x + this.size >= width) {
-      this.velX = -Math.abs(this.velX);
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
     }
 
-    if (this.x - this.size <= 0) {
-      this.velX = Math.abs(this.velX);
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
     }
 
-    if (this.y + this.size >= height) {
-      this.velY = -Math.abs(this.velY);
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
     }
 
-    if (this.y - this.size <= 0) {
-      this.velY = Math.abs(this.velY);
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
     }
 
     this.x += this.velX;
     this.y += this.velY;
   }
 
-  collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+  collisionDetect() {
+     for (const ball of balls) {
+        if (!(this === ball) && ball.exists) {
+           const dx = this.x - ball.x;
+           const dy = this.y - ball.y;
+           const distance = Math.sqrt(dx * dx + dy * dy);
+
+           if (distance < this.size + ball.size) {
+             ball.color = this.color = randomRGB();
+           }
         }
-      }
-    }
+     }
   }
+
 }
 
 const balls = [];
